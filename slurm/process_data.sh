0f960fb21d7ea58849ab8512163bbb8a6ae82f42
hieu nguyen <doanquanghieu.nguyen@gmail.com>
2025-03-02 00:00:36 -0600

    deep NN for clinical+rna

---
+++ slurm/process_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+#SBATCH --mail-user=nguyenhieu@uchicago.edu
+#SBATCH --mail-type=ALL
+#SBATCH --job-name=process_data 
+#SBATCH --chdir=/home/nguyenhieu/multimodal-ccRCC
+#SBATCH --output=/home/nguyenhieu/multimodal-ccRCC/slurm/out/%j.%N.stdout
+#SBATCH --error=/home/nguyenhieu/multimodal-ccRCC/slurm/out/%j.%N.stderr
+#SBATCH --partition=debug 
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem-per-cpu=900
+#SBATCH --exclusive
+
+python scripts/download_rna_seq.py
+python scripts/process_rna_seq.py
+python scripts/process_clinical.py
+python scripts/process_wsi.py
